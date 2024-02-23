804d4688b1c4231ea7ab25c887e97be23668ff09
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-02-23 18:46:01 +0530

    Varient added

--- src/pages/calender/index.jsx
+++ src/pages/calender/index.jsx
@@ -6,11 +6,11 @@ function Calender() {
   return (
     <main className="w-full xl:px-[48px] px-6 pb-6 xl:pb-[48px]pb-6 xl:pb-[48px] sm:pt-[156px] pt-[100px]">
       <div className="2xl:flex 2xl:space-x-[48px]">
-        <CalenderFull />
         <section className="2xl:w-[400px] w-full flex flex-col lg:flex-row 2xl:space-x-0 2xl:flex-col lg:space-x-6 space-x-0">
           <Wallet />
           <CalenderSm />
         </section>
+        <CalenderFull />
       </div>
     </main>
   );
