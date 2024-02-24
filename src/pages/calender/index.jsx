1feac8e4a0ab4c861e72caaec319ff03695cb859
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-02-24 19:55:46 +0530

    updated

--- src/pages/calender/index.jsx
+++ src/pages/calender/index.jsx
@@ -1,5 +1,4 @@
 import Wallet from "../../component/wallet";
-import CalenderSm from "../../component/calender";
 import CalenderFull from "../../component/calender/CalenderFull";
 
 function Calender() {
@@ -8,8 +7,6 @@ function Calender() {
       <div className="2xl:flex 2xl:space-x-[48px]">
         <CalenderFull />
         <section className="2xl:w-[400px] w-full flex flex-col lg:flex-row 2xl:space-x-0 2xl:flex-col lg:space-x-6 space-x-0">
-          <Wallet />
-          <CalenderSm />
         </section>
       </div>
     </main>
