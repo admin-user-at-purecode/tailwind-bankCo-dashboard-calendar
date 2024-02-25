645d48d40405b584848453bee5d6abc0f6988f2b
05Pratiksha <pratikshajaiswal10@gmail.com>
2024-02-25 12:49:52 +0530

    updated

--- src/pages/calender/index.jsx
+++ src/pages/calender/index.jsx
@@ -1,18 +1,16 @@
 import Wallet from "../../component/wallet";
-import CalenderSm from "../../component/calender";
 import CalenderFull from "../../component/calender/CalenderFull";
 
 function Calender() {
   return (
-    <main className="w-full xl:px-[48px] px-6 pb-6 xl:pb-[48px]pb-6 xl:pb-[48px] sm:pt-[156px] pt-[100px]">
-      <div className="2xl:flex 2xl:space-x-[48px]">
+    <>
+      <main className="w-full xl:px-[48px] px-6 pb-6 xl:pb-[48px]pb-6 xl:pb-[48px] sm:pt-[156px] pt-[100px]">
+        <Wallet />
+
+        <div className="2xl:flex 2xl:space-x-[48px] mb-5"></div>
         <CalenderFull />
-        <section className="2xl:w-[400px] w-full flex flex-col lg:flex-row 2xl:space-x-0 2xl:flex-col lg:space-x-6 space-x-0">
-          <Wallet />
-          <CalenderSm />
-        </section>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
